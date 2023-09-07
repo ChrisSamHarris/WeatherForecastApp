c94bcc41e9456f38f3127ff29369eb28afc1116d
ChrisSamHarris <christopherharris1995@ymail.com>
2023-09-07 10:04:59 +0100

    Plot sample data dynamically

--- Home.py
+++ Home.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.express as px
 
 st.set_page_config(page_title="Weather Forecast", layout="centered", page_icon="🌦️")
 
@@ -12,19 +13,21 @@ forecast_opt = st.selectbox("Data to view",('Temperature','Visibility'), help="C
 
 placeholder = st.empty()
 placeholder.subheader("Forecast will apply once you submit your location!")
-if loc is not '':
+
+if loc != '':
     placeholder.empty()
     if num_days > 1:
         placeholder.subheader(f"{forecast_opt} for the next {num_days} days in {loc.title()}")
     else:
         placeholder.subheader(f"{forecast_opt} for the next day in {loc.title()}")
 
-    chart_data = pd.DataFrame(
-        np.random.randn(20, 3),
-        columns = ['col1', 'col2', 'col3'])
+    def get_data(days):
+        fake_dates = ["07-09-2023", "08-09-2023","09-09-2023","10-09-2023","11-09-2023"]
+        fake_temp = [10, 15, 16, 18, 12]
+        fake_temp = [days * i for i in fake_temp]
+        return fake_dates, fake_temp   
+    
+    d, t = get_data(num_days)
 
-    st.line_chart(
-        chart_data,
-        x = 'col1',
-        y = ['col2', 'col3']
-    )
+    figure = px.line(x=d, y=t, labels={"x":"Date", "y":"Temperature °C"})
+    st.plotly_chart(figure)
