ce440793b20c17803ebc152e75cad8de4fc622bc
ChrisSamHarris <christopherharris1995@ymail.com>
2023-09-08 15:09:44 +0100

    Complete dynamic weather forecast, integration with API - works for any registered location in the world! Try/Except also provided to address any issues

--- Home.py
+++ Home.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 from backend import get_data
-import os
 
 
 st.set_page_config(page_title="Weather Forecast", layout="centered", page_icon="🌦️")
@@ -15,18 +14,47 @@ num_days = st.slider("Forecast Days",min_value=1, max_value=5, help="Select the 
 forecast_opt = st.selectbox("Data to view",('Temperature','Visibility'), help="Choose the temperature in °C or The visibility and sky conditions")
 
 placeholder = st.empty()
-placeholder.subheader("Forecast will apply once you submit your location!")
+placeholder.info("Forecast will apply once you submit your location!")
 
-if loc != '':
+if loc:
     placeholder.empty()
-    if num_days > 1:
-        placeholder.subheader(f"{forecast_opt} for the next {num_days} days in {loc.title()}")
-    else:
-        placeholder.subheader(f"{forecast_opt} for the next day in {loc.title()}")
+    try:
+        if num_days > 1:
+            placeholder.subheader(f"{forecast_opt} for the next {num_days} days in {loc.title()}")
+        else:
+            placeholder.subheader(f"{forecast_opt} for the next day in {loc.title()}")
 
-    data = get_data(loc, num_days, forecast_opt)
+        filtered_data = get_data(loc, num_days)
+
+        if filtered_data != "Invalid Location Provided":
+            dates = [dict["dt_txt"] for dict in filtered_data]
+
+        if forecast_opt == "Temperature":
+            temp_data_over_time = [round(dict['main']['temp'] - 273.15, 1) for dict in filtered_data]
+            
+            figure = px.line(x=dates, y=temp_data_over_time, labels={"x":"Date", "y":"Temperature °C"})
+            st.plotly_chart(figure)
+
+
+        if forecast_opt == "Visibility":
+            vis_data_over_time = [dict['weather'][0]['main'] for dict in filtered_data]
+            images = {"Clear": "visibility_images/clear.png",
+                    "Clouds": "visibility_images/cloud.png",
+                    "Rain": "visibility_images/rain.png",
+                    "Snow": "visibility_images/snow.png"}
+            
+            vis_image_paths = [images[condition] for condition in vis_data_over_time]
+            st.image(vis_image_paths, width=115, caption=dates)
     
-    d, t = get_data(num_days)
+    except KeyError:
+        placeholder.warning("Invalid Location provided!", icon="⚠️")
 
-    figure = px.line(x=d, y=t, labels={"x":"Date", "y":"Temperature °C"})
-    st.plotly_chart(figure)
+    except TypeError:
+        placeholder.warning("Invalid Location provided!", icon="⚠️")
+    
+    except Exception as e:
+        placeholder.warning("An error has occured, this has been reported internally. Please wait for this to be addressed.", icon="❗️")
+        print(f"Error, likely an invalid response/ API timeout\nError: {e}")
+
+# Two issues : 1 temp (addressed) - Error if a place doesnt exist? 
+        
--- backend.py
+++ backend.py
@@ -1,34 +1,26 @@
 import os
 import requests
-import json
-import datetime
 
 weather_api_key = os.getenv('WEATHER_API_KEY')
 
-def get_data(location, forecast_days, kind=None):
+def get_data(location, forecast_days):
     """Weather forecast for 5 days with data every 3 hours by city name. 8 data points per day."""
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={location}&appid={weather_api_key}"
     response = requests.get(url)
     data = response.json()
-
+    
     forecast_days = int(forecast_days) * 8
-    filtered_data = data['list']
-    filtered_data = filtered_data[:forecast_days]
 
-    # date_time = datetime.datetime.fromtimestamp(data['list'][0]['dt'])  
-    # print(data['list'][0]['dt'])
-    # print(str(date_time)[:10])
+    if data['cod'] == '404':
+        print("Invalid Location Provided")
+        return "Invalid Location Provided"
 
-    human_readable_data = json.dumps(data, indent =4 )
+    else:
+        filtered_data = data['list']
+        filtered_data = filtered_data[:forecast_days]
 
-    if kind == "Temperature":
-        filt_data_over_set_time = [dict['main']['temp'] for dict in filtered_data]
-    
-    if kind == "Visibility":
-        filt_data_over_set_time = [dict['weather'][0]['main'] for dict in filtered_data]
-    
-    print(filt_data_over_set_time)
-    return filt_data_over_set_time
+    return filtered_data
 
 if __name__ == "__main__":
-    get_data(location="Manchester", forecast_days=3, kind="Visibility")
+    data = get_data(location="New York", forecast_days=2)
+    print(data)
